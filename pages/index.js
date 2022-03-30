efc9f4620334c31738f1fddc001557def626a7bb
Frankcass <94424913+Frankcass@users.noreply.github.com>
2022-03-30 00:52:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,47 +11,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app JAF!" />
+        <Header title="Need to figure out the darn packages in this!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-import InfoComponent from '../../../components/InfoComponent'
-import Card from '../../../components/Card'
-// import Authors from './data.json'
-import {Authors} from '../../../data/data-containers/data-ConnectWallet'
-
-const CardSection = () => {
-
-  return (
-    <>
-	    <section className="section-padding-0-70 clearfix" >
-	        <div className="container">
-	            
-				<InfoComponent
-				  	titleSm='Explore Authors'
-				  	titleLg='Our Authors'
-				  	text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.'
-				/>
-	                
-
-	            <div className="row">
-	            	{Authors && Authors.map((item , i) => (
-	            		<Card
-	            			key={i}
-	            			imgBig={item.imgBig}
-	            			imgSm={item.imgSm}
-	            			name={item.name}
-	            		/>
-	            	))}
-	            </div>
-	        </div>
-	    </section>
-    </>
-  );
-}
-
-export default CardSection;
       <Footer />
     </div>
   )
